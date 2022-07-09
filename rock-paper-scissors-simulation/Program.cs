8e7d1ae43951638da1e15948c9cf23f9bd2cca19
admin <tobynwude@gmail.com>
2022-07-09 21:17:52 +0100

    first commit

---
+++ rock-paper-scissors-simulation/Program.cs
@@ -0,0 +1,164 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.Json;
+using rock_paper_scissors_simulation;
+
+Random random = new Random();
+Boolean playAgain = true;
+String playerA;
+String playerB;
+String answer;
+
+string _saveFileName = "todo.json";
+List<SavedGameModel> savedGames;
+string json;
+
+
+int Draws = 0;
+int PlayerAScore = 0;
+int PlayerBScore = 0;
+
+
+//while (playAgain) {
+for (int i = 0; i < 100; i++)
+{
+
+    playerA = "";
+    playerB = "";
+    answer = "";
+
+    playerA = "ROCK";
+
+    switch (random.Next(1, 4))
+    {
+        case 1:
+            playerB = "ROCK";
+            break;
+        case 2:
+            playerB = "PAPER";
+            break;
+        case 3:
+            playerB = "SCISSORS";
+            break;
+    }
+
+    Console.WriteLine("PlayerA: " + playerA);
+    Console.WriteLine("PlayerB: " + playerB);
+
+    switch (playerA)
+    {
+        case "ROCK":
+            if (playerB == "ROCK")
+            {
+                Console.WriteLine("It's a draw!");
+                Draws++;
+                savedGames = new List<SavedGameModel>
+                {
+                    new SavedGameModel { PlayerAWon = false, PlayerBWon = false, GameID = i },
+                };
+
+            }
+            else if (playerB == "PAPER")
+            {
+                Console.WriteLine("Player B Win!");
+                PlayerBScore++;
+                savedGames = new List<SavedGameModel>
+                {
+                    new SavedGameModel { PlayerAWon = false, PlayerBWon = true, GameID = i }, 
+                };
+            }
+            else
+            {
+                Console.WriteLine("Player A Win!");
+                PlayerAScore++;
+                savedGames = new List<SavedGameModel>
+                {
+                    new SavedGameModel { PlayerAWon = true, PlayerBWon = false, GameID = i },
+                };
+            }
+            break;
+        case "PAPER":
+            if (playerB == "ROCK")
+            {
+                Console.WriteLine("Player A Win!");
+                PlayerAScore++;
+                savedGames = new List<SavedGameModel>
+                {
+                    new SavedGameModel { PlayerAWon = true, PlayerBWon = false, GameID = i },
+                };
+            }
+            else if (playerB == "PAPER")
+            {
+                Console.WriteLine("It's a draw!");
+                Draws++;
+                savedGames = new List<SavedGameModel>
+                {
+                    new SavedGameModel { PlayerAWon = false, PlayerBWon = false, GameID = i },
+                };
+            }
+            else
+            {
+                Console.WriteLine("Player B Win!");
+                PlayerBScore++;
+                savedGames = new List<SavedGameModel>
+                {
+                    new SavedGameModel { PlayerAWon = false, PlayerBWon = true, GameID = i },
+                };
+            }
+            break;
+        case "SCISSORS":
+            if (playerB == "ROCK")
+            {
+                Console.WriteLine("Player B Win!");
+                PlayerBScore++;
+                savedGames = new List<SavedGameModel>
+                {
+                    new SavedGameModel { PlayerAWon = false, PlayerBWon= true, GameID = i },
+                };
+            }
+            else if (playerB == "PAPER")
+            {
+                Console.WriteLine("Player A Win!");
+                PlayerBScore++;
+                savedGames = new List<SavedGameModel>
+                {
+                    new SavedGameModel { PlayerAWon = true, PlayerBWon = false, GameID = i },
+                };
+            }
+            else
+            {
+                Console.WriteLine("It's a draw!");
+                Draws++;
+                savedGames = new List<SavedGameModel>
+                {
+                    new SavedGameModel { PlayerAWon = false, PlayerBWon = false, GameID = i },
+                };
+            }
+            
+
+            // Serialize it to JSON
+            json = JsonSerializer.Serialize(savedGames, new JsonSerializerOptions() { WriteIndented = true });
+
+            // Save it to a file
+            File.WriteAllText(_saveFileName, json);
+
+            break;
+    }
+
+
+}
+ json = File.ReadAllText(_saveFileName);
+
+List<SavedGameModel> items = JsonSerializer.Deserialize<List<SavedGameModel>>(json);
+
+// Check whether the list has loaded correctly
+foreach (var todo in items)
+    Console.WriteLine(todo);
+
+Console.WriteLine("Draws: " + Draws);
+Console.WriteLine("PlayerA: " + PlayerAScore);
+Console.WriteLine("PlayerB: " + PlayerBScore);
+
+
+Console.WriteLine("Thanks for Playing!");
+
---
+++ rock-paper-scissors-simulation/SavedGameModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace rock_paper_scissors_simulation
+{
+    public class SavedGameModel
+    {
+        public int GameID { get; set; }
+        public bool PlayerAWon { get; set; }
+        public bool PlayerBWon { get; set; }
+    }
+}
+
